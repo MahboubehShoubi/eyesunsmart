f904987bccfedccd54d9843b78ad9283c4a5ca01
Mahboubeh Shoubi <mahboobeh.shobi.65@gmail.com>
2024-10-16 03:35:32 -0700

    Creating a list of TisAirGateways menu items and changing the code structure to display the menus

--- src/components/module/tisairpage/TisAirGateways.js
+++ src/components/module/tisairpage/TisAirGateways.js
@@ -1,35 +1,35 @@
 import Image from "next/image";
 
-import 'animate.css';
-
+import "animate.css";
 
 function TisAirGateways() {
+  const GatewaysList = [
+    {
+      title: "Modbus VRF UPB Gateway",
+      Image: "/image/tis-group/tis-air/gateways/Modbus-VRF-UPB-Gateway.jpg",
+    },
+    {
+      title: "AIR BUS CONVERTER",
+      Image: "/image/tis-group/tis-air/gateways/AIR-BUS-CONVERTER.jpg",
+    },
+  ];
+
   return (
     <div className=" w-fit p-10 flex animate__animated animate__zoomIn">
-      <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
-        <div className="w-[150px] h-[250px] flex justify-center items-center">
-          <Image
-            src="/image/tis-group/tis-air/gateways/Modbus-VRF-UPB-Gateway.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
+      {GatewaysList.map((item) => (
+        <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
+          <div className="w-[150px] h-[250px] flex justify-center items-center">
+            <Image
+              src={item.Image}
+              className="w-full hover:scale-150 transition-all duration-[1500ms]"
+              width={1000}
+              height={700}
+              alt="panel-img"
+            />
+          </div>
+          <p>{item.title}</p>
         </div>
-        <p>Modbus VRF UPB Gateway</p>
-      </div>
-      <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
-        <div className="w-[150px] h-[250px] flex justify-center items-center">
-          <Image
-            src="/image/tis-group/tis-air/gateways/AIR-BUS-CONVERTER.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
-        </div>
-        <p>AIR BUS CONVERTER</p>
-      </div>
+      ))}
     </div>
   );
 }
