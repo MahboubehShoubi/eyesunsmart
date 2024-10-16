76203a0573b5b18783a603dbeae8d59ca456c411
Mahboubeh Shoubi <mahboobeh.shobi.65@gmail.com>
2024-10-16 03:01:49 -0700

    Creating a list of menu items and changing the code structure to display the menus

--- src/components/template/TisAirPage.js
+++ src/components/template/TisAirPage.js
@@ -11,6 +11,8 @@ import TisAirGateways from "@/module/tisairpage/TisAirGateways";
 function TisAirPage() {
   const [tisAir, setTisAir] = useState("panel");
 
+  const TisAirList = ["panel" , "controllers" , "sensors" , "intercom" , "gateways"]
+
   return (
     <div className="w-full mt-20 mb-20">
       <div className="container w-full md:max-w-screen-xl">
@@ -24,56 +26,16 @@ function TisAirPage() {
           />
         </div>
         <div className="w-full flex justify-around">
-          <button
+          {TisAirList.map(item => (<button
             className={
-              tisAir === "panel"
+              tisAir === `${item}`
                 ? "w-[150px] py-4 rounded-full bg-grayTisAir text-navyBlue "
                 : "w-[150px] py-4 rounded-full bg-blueTisAir text-navyBlue hover:bg-grayTisAir"
             }
-            onClick={(e) => setTisAir("panel")}
+            onClick={(e) => setTisAir(item)}
           >
-            Panel
-          </button>
-          <button
-            className={
-              tisAir === "controllers"
-                ? "w-[150px] py-4 rounded-full bg-grayTisAir text-navyBlue "
-                : "w-[150px] py-4 rounded-full bg-blueTisAir text-navyBlue hover:bg-grayTisAir"
-            }
-            onClick={(e) => setTisAir("controllers")}
-          >
-            Controllers
-          </button>
-          <button
-            className={
-              tisAir === "sensors"
-                ? "w-[150px] py-4 rounded-full bg-grayTisAir text-navyBlue "
-                : "w-[150px] py-4 rounded-full bg-blueTisAir text-navyBlue hover:bg-grayTisAir"
-            }
-            onClick={(e) => setTisAir("sensors")}
-          >
-            Sensors
-          </button>
-          <button
-            className={
-              tisAir === "intercom"
-                ? "w-[150px] py-4 rounded-full bg-grayTisAir text-navyBlue "
-                : "w-[150px] py-4 rounded-full bg-blueTisAir text-navyBlue hover:bg-grayTisAir"
-            }
-            onClick={(e) => setTisAir("intercom")}
-          >
-            Intercom
-          </button>
-          <button
-            className={
-              tisAir === "gateways"
-                ? "w-[150px] py-4 rounded-full bg-grayTisAir text-navyBlue "
-                : "w-[150px] py-4 rounded-full bg-blueTisAir text-navyBlue hover:bg-grayTisAir"
-            }
-            onClick={(e) => setTisAir("gateways")}
-          >
-            Gateways
-          </button>
+            {item}
+          </button>))}  
         </div>
         <div className="w-full flex justify-center">
           {tisAir === "panel" ? <TisAirPanel /> : null}
