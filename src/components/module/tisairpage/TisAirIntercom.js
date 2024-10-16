be95d187f1628a09c1e708c877b3506868397f20
Mahboubeh Shoubi <mahboobeh.shobi.65@gmail.com>
2024-10-16 03:41:35 -0700

    Creating a list of TisAirIntercom menu items and changing the structure of the codes to display the menus

--- src/components/module/tisairpage/TisAirIntercom.js
+++ src/components/module/tisairpage/TisAirIntercom.js
@@ -1,59 +1,42 @@
 import Image from "next/image";
 
-import 'animate.css';
-
+import "animate.css";
 
 function TisAirIntercom() {
+  const IntercomList = [
+    {
+      title: "7-Touch Screen",
+      Image: "/image/tis-group/tis-air/intercom/7-Touch-Screen.jpg",
+    },
+    {
+      title: "Outdoor Tower",
+      Image: "/image/tis-group/tis-air/intercom/Outdoor-Tower.jpg",
+    },
+    {
+      title: "Outdoor Building",
+      Image: "/image/tis-group/tis-air/intercom/Outdoor-Building.jpg",
+    },
+    {
+      title: "Outdoor Unit",
+      Image: "/image/tis-group/tis-air/intercom/Outdoor-Unit.jpg",
+    },
+  ];
   return (
     <div className=" w-fit p-10 flex animate__animated animate__zoomIn">
-      <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
-        <div className="w-[150px] h-[250px] flex justify-center items-center">
-          <Image
-            src="/image/tis-group/tis-air/intercom/7-Touch-Screen.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
+      {IntercomList.map((item) => (
+        <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
+          <div className="w-[150px] h-[250px] flex justify-center items-center">
+            <Image
+              src={item.Image}
+              className="w-full hover:scale-150 transition-all duration-[1500ms]"
+              width={1000}
+              height={700}
+              alt="panel-img"
+            />
+          </div>
+          <p>{item.title}</p>
         </div>
-        <p>7-Touch Screen</p>
-      </div>
-      <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
-        <div className="w-[150px] h-[250px] flex justify-center items-center">
-          <Image
-            src="/image/tis-group/tis-air/intercom/Outdoor-Tower.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
-        </div>
-        <p>Outdoor Tower</p>
-      </div>
-      <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
-        <div className="w-[150px] h-[250px] flex justify-center items-center">
-          <Image
-            src="/image/tis-group/tis-air/intercom/Outdoor-Building.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
-        </div>
-        <p>Outdoor Building</p>
-      </div>
-      <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
-        <div className="w-[150px] h-[250px] flex justify-center items-center">
-          <Image
-            src="/image/tis-group/tis-air/intercom/Outdoor-Unit.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
-        </div>
-        <p>Outdoor Unit</p>
-      </div>
+      ))}
     </div>
   );
 }
