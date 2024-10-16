949be44f84b9aab7bd0f056f220ad87e0425d04a
Mahboubeh Shoubi <mahboobeh.shobi.65@gmail.com>
2024-10-16 03:29:00 -0700

    Creating a list of TisAirControllers menu items and changing the code structure to display the menus

--- src/components/module/tisairpage/TisAirControllers.js
+++ src/components/module/tisairpage/TisAirControllers.js
@@ -1,71 +1,46 @@
 import Image from "next/image";
 
-import 'animate.css';
-
+import "animate.css";
 
 function TisAirControllers() {
+  const ControllersList = [
+    {
+      title: "ADS-Controller",
+      Image: "/image/tis-group/tis-air/controllers/ADS-Controller.jpg",
+    },
+    {
+      title: "ACM Switch",
+      Image: "/image/tis-group/tis-air/controllers/ACM-Switch.jpg",
+    },
+    {
+      title: "AIR Socket",
+      Image: "/image/tis-group/tis-air/controllers/AIR-Socket.jpg",
+    },
+    {
+      title: "AIR IR Emitter",
+      Image: "/image/tis-group/tis-air/controllers/AIR-IR-Emitter.jpg",
+    },
+    {
+      title: "TRV Controller",
+      Image: "/image/tis-group/tis-air/controllers/TRV-Controller.jpg",
+    },
+  ];
   return (
     <div className=" w-fit p-10 flex animate__animated animate__zoomIn">
-      <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
-        <div className="w-[150px] h-[250px] flex justify-center items-center">
-          <Image
-            src="/image/tis-group/tis-air/controllers/ADS-Controller.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
+      {ControllersList.map((item) => (
+        <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
+          <div className="w-[150px] h-[250px] flex justify-center items-center">
+            <Image
+              src={item.Image}
+              className="w-full hover:scale-150 transition-all duration-[1500ms]"
+              width={1000}
+              height={700}
+              alt="panel-img"
+            />
+          </div>
+          <p>{item.title}</p>
         </div>
-        <p>ADS-Controller</p>
-      </div>
-      <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
-        <div className="w-[150px] h-[250px] flex justify-center items-center">
-          <Image
-            src="/image/tis-group/tis-air/controllers/ACM-Switch.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
-        </div>
-        <p>ACM Switch</p>
-      </div>
-      <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
-        <div className="w-[150px] h-[250px] flex justify-center items-center">
-          <Image
-            src="/image/tis-group/tis-air/controllers/AIR-Socket.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
-        </div>
-        <p>AIR Socket</p>
-      </div>
-      <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
-        <div className="w-[150px] h-[250px] flex justify-center items-center">
-          <Image
-            src="/image/tis-group/tis-air/controllers/AIR-IR-Emitter.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
-        </div>
-        <p>AIR IR Emitter</p>
-      </div>
-      <div className="w-fit p-5 flex flex-col items-center hover:cursor-pointer">
-        <div className="w-[150px] h-[250px] flex justify-center items-center">
-          <Image
-            src="/image/tis-group/tis-air/controllers/TRV-Controller.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
-        </div>
-        <p>TRV Controller</p>
-      </div>
+      ))}
     </div>
   );
 }
