ffd801a554fcc9bf57ab5c0d080e84e335e13e83
Mahboubeh Shoubi <mahboobeh.shobi.65@gmail.com>
2024-10-16 03:18:54 -0700

    Create a list of TisAirPanel menu items and change the code structure to display the menus

--- src/components/module/tisairpage/TisAirPanel.js
+++ src/components/module/tisairpage/TisAirPanel.js
@@ -1,47 +1,39 @@
 import Image from "next/image";
 
-import 'animate.css';
-
+import "animate.css";
 
 function TisAirPanel() {
+  const PanelList = [
+    {
+      title: "Venera Switch",
+      Image: "/image/tis-group/tis-air/panel/venera-switch.jpg",
+    },
+    {
+      title: "Venera Dimmer",
+      Image: "/image/tis-group/tis-air/panel/venera-dimmer.jpg",
+    },
+    {
+      title: "Venera Thermostat",
+      Image: "/image/tis-group/tis-air/panel/venera-thermostat.jpg",
+    },
+  ];
+
   return (
     <div className=" w-fit p-10 flex animate__animated animate__zoomIn">
-      <div className="w-fit p-5 flex flex-col items-center">
-        <div className="w-[150px] h-[250px] flex justify-center items-center hover:cursor-pointer">
-          <Image
-            src="/image/tis-group/tis-air/panel/venera-switch.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
+      {PanelList.map((item) => (
+        <div className="w-fit p-5 flex flex-col items-center">
+          <div className="w-[150px] h-[250px] flex justify-center items-center hover:cursor-pointer">
+            <Image
+              src={item.Image}
+              className="w-full hover:scale-150 transition-all duration-[1500ms]"
+              width={1000}
+              height={700}
+              alt="panel-img"
+            />
+          </div>
+          <p>{item.title}</p>
         </div>
-        <p>Venera Switch</p>
-      </div>
-      <div className="w-fit p-5 flex flex-col items-center">
-        <div className="w-[150px] h-[250px] flex justify-center items-center hover:cursor-pointer">
-          <Image
-            src="/image/tis-group/tis-air/panel/venera-dimmer.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
-        </div>
-        <p>Venera Dimmer</p>
-      </div>
-      <div className="w-fit p-5 flex flex-col items-center">
-        <div className="w-[150px] h-[250px] flex justify-center items-center hover:cursor-pointer">
-          <Image
-            src="/image/tis-group/tis-air/panel/venera-thermostat.jpg"
-            className="w-full hover:scale-150 transition-all duration-[1500ms]"
-            width={1000}
-            height={700}
-            alt="panel-img"
-          />
-        </div>
-        <p>Venera Thermostat</p>
-      </div>
+      ))}
     </div>
   );
 }
