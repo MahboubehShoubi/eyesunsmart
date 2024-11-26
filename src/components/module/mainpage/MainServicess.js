0ed464963477a1142ac905c7ce17bc43797a1dbb
Mahboubeh Shoubi <mahboobeh.shobi.65@gmail.com>
2024-11-26 03:39:03 -0800

    Changing the content of the main page of the site in MainServicess.js, MainSettingup.js sections

--- src/components/module/mainpage/MainServicess.js
+++ src/components/module/mainpage/MainServicess.js
@@ -10,21 +10,35 @@ import { BsLampFill } from "react-icons/bs";
 import { GiDeskLamp } from "react-icons/gi";
 import { FaEye } from "react-icons/fa";
 
+import { MdCastForEducation } from "react-icons/md";
+import { MdOutlineOnDeviceTraining } from "react-icons/md";
+import { RiCustomerService2Fill } from "react-icons/ri";
+import { MdLaptopMac } from "react-icons/md";
+import { BsFillHouseGearFill } from "react-icons/bs";
+import { FaShopify } from "react-icons/fa";
+
+
+
 function MainServicess() {
   return (
-    <div className=" relative w-full mt-[50px] md:mt-[100px] ">
-      <div className=" container md:max-w-screen-xl relative md:h-[115vh] grid md:grid-rows-5">
-        <div className="w-full  grid grid-cols-1 md:grid-cols-12 md:grid-rows-[5vh_50vh_40vh_20vh]">
+    <div className=" relative w-full mt-[50px] md:mt-[100px] h-fit">
+      <div className=" container md:max-w-screen-xl relative md:h-[150vh] grid md:grid-rows-5">
+        <div className="w-full md:row-start-1 md:row-end-6 grid grid-cols-1 md:grid-cols-12 md:grid-rows-[7vh_60vh_50vh_33vh] ">
           <div className=" md:col-start-1 md:col-end-8 md:row-start-2 md:row-end-3 rounded-[60px] md:rounded-l-[0] bg-bgRed p-[40px]">
-            <span className="text-textWhite text-[1.1rem] md:text-titleFontSize ">خدمات ما</span>
-            <h2 className=" text-[1.5rem] md:text-mainTitleFontSize text-textWhite">
-              Smart Solutions, Seamless Living.
+            <span className="text-textWhite text-[1.1rem] md:text-titleFontSize border-b-2">
+              خدمات ما
+            </span>
+            <h2 className=" text-[1.5rem] md:text-mainTitleFontSize text-textWhite mt-5">
+              خدمات خانه هوشمند
             </h2>
-            <p className="text-[14px] md:text-[1rem]">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
-              در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد
-              نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
+            <h2 className=" text-[1.5rem] md:text-mainTitleFontSize text-textWhite">
+            تجربه زندگی مدرن و هوشمند
+            </h2>
+            <p className="text-[13px] md:text-[1rem] text-textWhite mt-5">
+              خانه شما شایسته بهترین‌هاست! خدمات خانه هوشمند ما طراحی شده‌اند تا
+              آسایش، امنیت و بهره‌وری را به زندگی شما بیاورند. از طراحی و نصب
+              گرفته تا پشتیبانی و آموزش، تمامی نیازهای شما برای داشتن یک خانه
+              هوشمند در یک مکان ارائه می‌شود.
             </p>
             <div className="md:hidden rounded-[30px] overflow-hidden mt-10">
               <Image
@@ -49,49 +63,69 @@ function MainServicess() {
 
         <div className="relative md:absolute z-[2] row-start-3 row-end-6 w-full grid grid-cols-1 gap-8 md:grid-cols-3 md:grid-rows-2 justify-between content-between px-[20px] md:px-[50px]">
           <div className=" md:col-start-1 md:col-span-1 md:row-start-1 md:row-span-1 bg-backgroundWhite rounded-3xl px-[30px] py-[30px] hover:shadow-lg">
-            <MdOutlineMobileScreenShare className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" />
-            <h3 className=" text-[1.3rem] md:text-subTitle text-navyBlue">نصب دستگاه هوشمند</h3>
+            {/* <MdOutlineMobileScreenShare className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" /> */}
+            <FaShopify className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" />
+            <h3 className=" text-[1.3rem] md:text-subTitle text-navyBlue">
+              فروش سیستم های هوشمند
+            </h3>
             <p className="text-textGray text-[14px]">
-              للورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
-              در ستون و سطرآنچنان که لازم است
+              آیا می‌خواهید راحتی، امنیت و بهره‌وری را به خانه خود بیاورید؟
+              سیستم‌های هوشمند ما طراحی شده‌اند تا زندگی شما را آسان‌تر و
+              مدرن‌تر کنند.{" "}
             </p>
           </div>
           <div className=" md:col-start-2 md:col-span-1 md:row-start-1 md:row-span-1 bg-backgroundWhite rounded-3xl px-[30px] py-[30px] hover:shadow-lg">
-            <GiAutomaticSas className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" />
-            <h3 className="text-[1.3rem] md:text-subTitle text-navyBlue">نصب دستگاه هوشمند</h3>
+            {/* <GiAutomaticSas className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" /> */}
+            <BsFillHouseGearFill  className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" />
+            <h3 className="text-[1.3rem] md:text-subTitle text-navyBlue">
+              نصب و راه اندازی سیستم های هوشمند
+            </h3>
             <p className="text-textGray text-[14px]">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
-              در ستون و سطرآنچنان که لازم است
+              راه‌اندازی سیستم‌های هوشمند شاید پیچیده به نظر برسد، اما با خدمات
+              حرفه‌ای ما، شما تنها چند قدم با خانه‌ای مدرن و هوشمند فاصله دارید.
+              تیم ما از ابتدا تا انتها همراه شماست تا تجربه‌ای بدون دردسر و
+              کاملاً مطمئن داشته باشید.{" "}
             </p>
           </div>
 
           <div className=" md:col-start-1 md:col-span-1 md:row-start-2 md:row-span-1 bg-backgroundWhite rounded-3xl px-[30px] py-[30px] hover:shadow-lg">
-            <BsLampFill className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" />
-            <h3 className="text-[1.3rem] md:text-subTitle text-navyBlue">نصب دستگاه هوشمند</h3>
+            {/* <BsLampFill className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" /> */}
+            <MdLaptopMac className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" />
+            <h3 className="text-[1.3rem] md:text-subTitle text-navyBlue">
+              برنامه نویسی سیستم های هوشمند
+            </h3>
             <p className="text-textGray text-[14px]">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
-              در ستون و سطرآنچنان که لازم است
+              خانه هوشمند شما باید دقیقاً مطابق با نیازها و سبک زندگی شما عمل
+              کند. به همین دلیل، ما خدمات برنامه‌نویسی سفارشی را ارائه می‌دهیم
+              تا عملکرد سیستم‌های هوشمند را به طور کامل شخصی‌سازی کنید.{" "}
             </p>
           </div>
           <div className=" md:col-start-2 md:col-span-1 md:row-start-2 md:row-span-1 bg-backgroundWhite rounded-3xl px-[30px] py-[30px] hover:shadow-lg">
-            <FaEye className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" />
-            <h3 className="text-[1.3rem] md:text-subTitle text-navyBlue">نصب دستگاه هوشمند</h3>
+            {/* <FaEye className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" /> */}
+            <RiCustomerService2Fill className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" />
+            <h3 className="text-[1.3rem] md:text-subTitle text-navyBlue">
+              خدمات پس از فروش
+            </h3>
             <p className="text-textGray text-[14px]">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
-              در ستون و سطرآنچنان که لازم است
+              خانه هوشمند تنها با خرید تجهیزات به پایان نمی‌رسد؛ پشتیبانی و
+              خدمات پس از فروش حرفه‌ای بخش جدایی‌ناپذیر تجربه‌ای مطمئن و لذت‌بخش
+              است. ما در کنار شما هستیم تا از عملکرد بی‌نقص سیستم‌های هوشمند
+              خانه‌تان اطمینان حاصل کنید.{" "}
             </p>
           </div>
           <div className=" md:col-start-3 md:col-span-1 md:row-start-2 md:row-span-1 bg-backgroundWhite rounded-3xl px-[30px] py-[30px] hover:shadow-lg">
-            <GiDeskLamp className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" />
-            <h3 className="text-subTitle text-navyBlue">نصب دستگاه هوشمند</h3>
+            {/* <GiDeskLamp className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" /> */}
+            {/* <MdCastForEducation className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" /> */}
+            <MdOutlineMobileScreenShare className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" />
+            {/* <MdOutlineOnDeviceTraining className="w-[55px] h-[55px] bg-navyBlue text-textWhite p-2 rounded-full hover:bg-secondery cursor-pointer" /> */}
+            <h3 className="text-subTitle text-navyBlue">
+              آموزش سیستم های هوشمند
+            </h3>
             <p className="text-textGray text-[14px]">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
-              در ستون و سطرآنچنان که لازم است
+              خانه هوشمند شما مجهز به تکنولوژی‌های پیشرفته است، اما برای استفاده
+              بهینه از امکانات آن، آموزش صحیح ضروری است. ما در کنار شما هستیم تا
+              تمامی جنبه‌های استفاده از سیستم‌های هوشمند را به زبانی ساده و
+              کاربردی آموزش دهیم.{" "}
             </p>
           </div>
         </div>
--- src/components/module/mainpage/MainSettingUp.js
+++ src/components/module/mainpage/MainSettingUp.js
@@ -11,64 +11,80 @@ function MainSettingUp() {
         <div className="relative w-full h-fit mt-10 flex justify-center">
           <div className=" hidden md:block md:absolute md:z-[-1] md:w-[80%] md:h-10 md:border-b-4 md:border-dotted md:border-indigo-600"></div>
           <div className="w-full flex flex-col gap-y-11 md:flex-row justify-between items-center">
-            <div className="w-[70%] md:w-[22.5%] flex flex-col items-center gap-y-5 md:gap-y-8">
+            <div className="w-[70%] md:w-[19%] flex flex-col items-center gap-y-5 md:gap-y-8">
               <div className=" w-[70px] h-[70px] flex justify-center items-center bg-bgRed rounded-full outline outline-offset-2 outline-4 outline-bgRed">
                 <span className="bg-bgRed text-textWhite text-[2rem]">01</span>
               </div>
               <div className="text-center">
-                <h4 className="text-[1.2rem] font-bold">
-                  فقط وصل کنید و بازی کنید
+                <h4 className="text-[1.2rem] font-semibold">
+                  مشاوره و طراحی سفارشی{" "}
                 </h4>
-                <p className="mt-3">
-                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                  استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و
-                  مجله در ستون و سطرآنچنان که لازم است
+                <p className="mt-3 font-light">
+                  هر خانه و هر مشتری منحصربه‌فرد است. تیم متخصص ما پس از بررسی
+                  نیازها و زیرساخت‌های خانه شما، راهکارهایی متناسب با سبک زندگی
+                  و انتظارات شما طراحی می‌کند.{" "}
                 </p>
               </div>
             </div>
 
-            <div className="w-[70%] md:w-[22.5%] flex flex-col items-center gap-y-5 md:gap-y-8">
+            <div className="w-[70%] md:w-[19%] flex flex-col items-center gap-y-5 md:gap-y-8">
               <div className=" w-[70px] h-[70px] flex justify-center items-center bg-bgRed rounded-full outline outline-offset-2 outline-4 outline-bgRed">
                 <span className="bg-bgRed text-textWhite text-[2rem]">02</span>
               </div>
               <div className="text-center">
-                <h4 className="text-[1.2rem] font-bold">
-                  اتصال و تنظیم در گوشی هوشمند
+                <h4 className="text-[1.2rem] font-semibold">
+                  نصب و راه‌اندازی حرفه‌ای{" "}
                 </h4>
-                <p className="mt-3">
-                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                  استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و
-                  مجله در ستون و سطرآنچنان که لازم است
+                <p className="mt-3 font-light">
+                  نصب سیستم‌های هوشمند باید به دقت و با مهارت انجام شود. ما
+                  تضمین می‌کنیم تمامی تجهیزات شما با بالاترین کیفیت و به شکلی
+                  کاملاً یکپارچه نصب و راه‌اندازی شوند.{" "}
                 </p>
               </div>
             </div>
 
-            <div className="w-[70%] md:w-[22.5%] flex flex-col items-center gap-y-5 md:gap-y-8">
+            <div className="w-[70%] md:w-[19%] flex flex-col items-center gap-y-5 md:gap-y-8">
               <div className=" w-[70px] h-[70px] flex justify-center items-center bg-bgRed rounded-full outline outline-offset-2 outline-4 outline-bgRed">
                 <span className="bg-bgRed text-textWhite text-[2rem]">03</span>
               </div>
               <div className="text-center">
-                <h4 className="text-[1.1rem] font-bold">
-                  کاری را که می خواهید انجام دهید انتخاب کنید
+                <h4 className="text-[1.1rem] font-semibold">
+                  برنامه‌نویسی و شخصی‌سازی{" "}
                 </h4>
-                <p className="mt-3">
-                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                  استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و
-                  مجله در ستون و سطرآنچنان که لازم است
+                <p className="mt-3 font-light">
+                  خانه هوشمند شما باید دقیقاً مطابق با نیازهای شما عمل کند. با
+                  خدمات برنامه‌نویسی ما، می‌توانید سناریوهای خاص و تنظیمات
+                  دلخواه خود را برای تجهیزات هوشمندتان تعریف کنید.{" "}
                 </p>
               </div>
             </div>
 
-            <div className="w-[70%] md:w-[22.5%] flex flex-col items-center gap-y-5 md:gap-y-8">
+            <div className="w-[70%] md:w-[19%] flex flex-col items-center gap-y-5 md:gap-y-8">
               <div className=" w-[70px] h-[70px] flex justify-center items-center bg-bgRed rounded-full outline outline-offset-2 outline-4 outline-bgRed">
                 <span className="bg-bgRed text-textWhite text-[2rem]">04</span>
               </div>
               <div className="text-center">
-                <h4 className="text-[1.2rem] font-bold"> بازی کنید</h4>
-                <p className="mt-3">
-                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                  استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و
-                  مجله در ستون و سطرآنچنان که لازم است
+                <h4 className="text-[1.2rem] font-semibold">آموزش و پشتیبانی</h4>
+                <p className="mt-3 font-light">
+                  فناوری تنها زمانی مفید است که به درستی از آن استفاده شود. ما
+                  تمامی آموزش‌های لازم برای بهره‌گیری از امکانات خانه هوشمند را
+                  به شما ارائه می‌دهیم و در هر مرحله از استفاده، کنار شما خواهیم
+                  بود.{" "}
+                </p>
+              </div>
+            </div>
+            <div className="w-[70%] md:w-[19%] flex flex-col items-center gap-y-5 md:gap-y-8">
+              <div className=" w-[70px] h-[70px] flex justify-center items-center bg-bgRed rounded-full outline outline-offset-2 outline-4 outline-bgRed">
+                <span className="bg-bgRed text-textWhite text-[2rem]">05</span>
+              </div>
+              <div className="text-center">
+                <h4 className="text-[1.2rem] font-semibold">
+                  به‌روزرسانی و خدمات پس از فروش
+                </h4>
+                <p className="mt-3 font-light">
+                  فناوری هوشمند دائماً در حال پیشرفت است. خدمات ما شامل
+                  به‌روزرسانی نرم‌افزارها، ارتقای سیستم‌ها و پشتیبانی 24/7 برای
+                  رفع هرگونه مشکل احتمالی است.{" "}
                 </p>
               </div>
             </div>
