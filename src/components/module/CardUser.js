1efae4618f863505d27e4c9a0ad5cdcf10fa880b
Mahboubeh Shoubi <mahboobeh.shobi.65@gmail.com>
2025-01-24 05:11:58 -0800

    Making changes in the display style of information received from the server in the CardUser.js component

--- src/components/module/CardUser.js
+++ src/components/module/CardUser.js
@@ -1,189 +1,279 @@
 "use client";
 import { useEffect, useState } from "react";
 
+//Icons
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
 import { MdDeleteForever } from "react-icons/md";
 import { IoMdCheckboxOutline } from "react-icons/io";
 import { CiEdit } from "react-icons/ci";
 
-function CardUser({ userData }) {
-  
-  const [user, setUser] = useState({
-    id: "",
-    fullName: "",
-    phone: "",
-    email: "",
-    idCode: "",
-    job: "",
-    age: "",
-    province: "",
-    city: "",
-    role: "",
-    status: "",
-  });
+import "animate.css";
 
-  useEffect(() => {
-    setUser({
-      id: userData.id,
-      fullName: userData.fullName,
-      phone: userData.phone,
-      email: userData.email,
-      idCode: userData.idCode,
-      job: userData.job,
-      age: userData.age,
-      province: userData.province,
-      city: userData.city,
-      role: userData.role,
-      status: userData.status,
-    });
-  }, []);
+function CardUser({ userData }) {
+  const [role, setRole] = useState(userData.role);
+  const [showDetails, setShowDetails] = useState(false); // کنترل نمایش اطلاعات اضافی
 
   const changeHandler = (e) => {
-    const { name, value } = e.target;
-      setUser({ ...user, [name]: value });
+    setRole(e.target.value);
   };
 
-  const finalHandler = (id) => {
-    setUser({ ...user, ["status"]: "complate" });
+  const toggleDetails = () => {
+    setShowDetails((prev) => !prev);
   };
+
+  // ارسال تغییرات به سرور
+  const saveRoleToDatabase = async () => {
+    const body = {
+      id: userData.id,
+      role,
+      status: "complate", // وضعیت جدید
+    };
+    console.log(body);
+
+    // try {
+    //   const response = await fetch(`/api/update-role`, {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(body),
+    //   });
+
+    //   if (response.ok) {
+    //     alert("Role updated successfully!");
+    //   } else {
+    //     alert("Failed to update role.");
+    //   }
+    // } catch (error) {
+    //   console.error("Error updating role:", error);
+    //   alert("An error occurred while updating role.");
+    // }
+  };
+
+  const deleteUserHandler = () => {
+    const userDeletedId = userData.id;
+    console.log("deleted user : ", userDeletedId);
+  };
+
+  const editUserHandler = () => {};
 
   return (
-    <div className="px-5 py-4 rounded-[10px] shadow-[0px_0px_20px_0px_rgba(0,0,0,0.3)]">
-      <div className=" w-full grid grid-cols-2 md:grid-cols-3 gap-5">
-        <div className="flex gap-x-2 ">
-          <label htmlFor="" className="text-bgRed ">
-            نام و نام خانوادگی :{" "}
-          </label>
-          <p>{user.fullName}</p>
+    <div className="px-5 py-8 rounded-[10px] shadow-[0px_0px_20px_0px_rgba(0,0,0,0.3)]">
+      {/* اطلاعات اصلی */}
+      <div className="grid grid-cols-2 sm:grid-cols-7 gap-4 items-center">
+        <div className="flex gap-x-2 col-span-2 ">
+          <label className="text-bgRed">نام و نام خانوادگی:</label>
+          <p>{userData.fullName}</p>
         </div>
-        <div className="flex gap-x-2">
-          <label htmlFor="" className="text-bgRed ">
-            کد ملی :
-          </label>
-          <p>{user.idCode} </p>
+        <div className="flex gap-x-2 col-span-2">
+          <label className="text-bgRed">کد ملی:</label>
+          <p>{userData.idCode}</p>
         </div>
-        <div className="flex gap-x-2">
-          <label htmlFor="" className="text-bgRed ">
-            شماره تماس :
-          </label>
-          <p>{user.phone} </p>
+        <div className="flex gap-x-2 col-span-2">
+          <label className="text-bgRed">شماره تماس:</label>
+          <p>{userData.phone}</p>
         </div>
-        <div className="flex gap-x-2">
-          <label htmlFor="" className="text-bgRed ">
-            شغل :
-          </label>
-          <p>{user.job}</p>
-        </div>
-        <div className="flex gap-x-2">
-          <label htmlFor="" className="text-bgRed ">
-            استان :
-          </label>
-          <p>{user.province}</p>
-        </div>
-        <div className="flex gap-x-2">
-          <label htmlFor="" className="text-bgRed ">
-            شهر :
-          </label>
-          <p>{user.city}</p>
-        </div>
-        <div className="flex gap-x-2">
-          <label htmlFor="" className="text-bgRed ">
-            سن :
-          </label>
-          <p>{user.age}</p>
-        </div>
-      </div>
-      <div className=" flex items-baseline">
-        <p className="w-[150px] text-bgRed"> تعیین وضعیت :</p>
-        <div className="w-full flex md:justify-between md:items-end md:pl-20">
-          <div className="w-fit flex gap-x-3">
-            <label htmlFor="Admin" className="">
-              ادمین سایت
-            </label>
-            <input
-              type="radio"
-              name="role"
-              id="Admin"
-              value="ADMIN"
-              onChange={(e) => changeHandler(e)}
-              checked={user.role === "ADMIN"}
-            />
-          </div>
-
-          <div className="w-fit flex gap-x-2">
-            <label htmlFor="User" className="">
-              کاربر عادی
-            </label>
-            <input
-              type="radio"
-              name="role"
-              id="User"
-              value="USER"
-              onChange={(e) => changeHandler(e)}
-              checked={user.role === "USER"}
-            />
-          </div>
-
-          <div className="w-fit flex gap-x-2">
-            <label htmlFor="Customer" className="">
-              باشگاه مشتریان
-            </label>
-            <input
-              type="radio"
-              name="role"
-              id="Customer"
-              value="CUSTOMER"
-              onChange={(e) => changeHandler(e)}
-              checked={user.role === "CUSTOMER"}
-            />
-          </div>
-
-          <div className="w-fit flex gap-x-2">
-            <label htmlFor="Colleague" className="">
-              همکار
-            </label>
-            <input
-              type="radio"
-              name="role"
-              id="Colleague"
-              value="COLLEAGUE"
-              onChange={(e) => changeHandler(e)}
-              checked={user.role === "COLLEAGUE"}
-            />
-          </div>
-        </div>
-      </div>
-      {user.status === "pendding" ? (
-        <div className="mt-10 flex gap-x-[5%] ">
+        {/* دکمه نمایش جزئیات */}
+        <div className="flex justify-end col-span-1">
           <button
-            className="w-1/2 bg-green text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center"
-            onClick={() => finalHandler(user.id)}
+            onClick={toggleDetails}
+            className="rounded-[5px] bg-primaryBlue text-textWhite w-full py-[5px] text-[12px] flex gap-x-1 justify-center items-center"
           >
-            <IoMdCheckboxOutline className="w-[22px] h-[22px]" />
-            تائید نهایی
-          </button>
-          <button className="w-1/2 bg-bgRed text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center">
-            <MdDeleteForever className="w-[22px] h-[22px]" />
-            حذف کاربر
-          </button>
-        </div>
-      ) : (
-        <div className="mt-10 flex gap-x-[5%] ">
-          <button className="w-1/3 bg-green text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center">
-            <IoMdCheckboxOutline className="w-[22px] h-[22px]" />
-            تائید نهایی
-          </button>
-          <button className="w-1/3 bg-yellowTisBee text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center">
-            <CiEdit className="w-[22px] h-[22px]" />
-            ویرایش اطلاعات
-          </button>
-          <button className="w-1/3 bg-bgRed text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center">
-            <MdDeleteForever className="w-[22px] h-[22px]" />
-            حذف کاربر
+            {showDetails ? "نمایش کمتر" : "نمایش بیشتر"}
+            {showDetails ? (
+              <IoIosArrowUp className="w-[15px] h-[15px]" />
+            ) : (
+              <IoIosArrowDown className="w-[15px] h-[15px]" />
+            )}
           </button>
         </div>
-      )}
+      </div>
+
+      {/* اطلاعات اضافی */}
+      <div
+        className={`transition-max-height duration-[1s] ease-in-out overflow-hidden ${
+          showDetails ? "max-h-[250px]" : "max-h-0"
+        }`}
+      >
+        <div className="grid grid-cols-2 sm:grid-cols-7 gap-4 mt-5 items-center">
+          <div className="flex gap-x-2 col-span-2">
+            <label className="text-bgRed">شغل:</label>
+            <p>{userData.job}</p>
+          </div>
+          <div className="flex gap-x-2 col-span-2">
+            <label className="text-bgRed">استان:</label>
+            <p>{userData.province}</p>
+          </div>
+          <div className="flex gap-x-2 col-span-2">
+            <label className="text-bgRed">شهر:</label>
+            <p>{userData.city}</p>
+          </div>
+          <div className="flex gap-x-2 col-span-2">
+            <label className="text-bgRed">سن:</label>
+            <p>{userData.age}</p>
+          </div>
+        </div>
+
+        <div className="flex items-baseline mt-4">
+          <p className="w-[150px] text-bgRed">تعیین وضعیت:</p>
+          <div className="w-full flex md:justify-between md:items-end md:pl-20">
+            <div className="w-fit flex gap-x-3">
+              <label htmlFor={`Admin-${userData.id}`}>
+                <input
+                  type="radio"
+                  name={`role-${userData.id}`}
+                  id={`Admin-${userData.id}`}
+                  value="ADMIN"
+                  onChange={changeHandler}
+                  checked={role === "ADMIN"}
+                />
+                ادمین سایت
+              </label>
+            </div>
+            <div className="w-fit flex gap-x-2">
+              <label htmlFor={`User-${userData.id}`}>
+                <input
+                  type="radio"
+                  name={`role-${userData.id}`}
+                  id={`User-${userData.id}`}
+                  value="USER"
+                  onChange={changeHandler}
+                  checked={role === "USER"}
+                />
+                کاربر عادی
+              </label>
+            </div>
+            <div className="w-fit flex gap-x-2">
+              <label htmlFor={`Customer-${userData.id}`}>
+                <input
+                  type="radio"
+                  name={`role-${userData.id}`}
+                  id={`Customer-${userData.id}`}
+                  value="CUSTOMER"
+                  onChange={changeHandler}
+                  checked={role === "CUSTOMER"}
+                />
+                باشگاه مشتریان
+              </label>
+            </div>
+            <div className="w-fit flex gap-x-2">
+              <label htmlFor={`Colleague-${userData.id}`}>
+                <input
+                  type="radio"
+                  name={`role-${userData.id}`}
+                  id={`Colleague-${userData.id}`}
+                  value="COLLEAGUE"
+                  onChange={changeHandler}
+                  checked={role === "COLLEAGUE"}
+                />
+                همکار
+              </label>
+            </div>
+          </div>
+        </div>
+
+        {userData.status === "pendding" ? (
+          <div className="mt-10 flex gap-x-[5%] ">
+            <button
+              className="w-1/2 bg-green text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center"
+              onClick={saveRoleToDatabase}
+            >
+              <IoMdCheckboxOutline className="w-[22px] h-[22px]" />
+              تائید نهایی
+            </button>
+            <button
+              className="w-1/2 bg-bgRed text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center"
+              onClick={deleteUserHandler}
+            >
+              <MdDeleteForever className="w-[22px] h-[22px]" />
+              حذف کاربر
+            </button>
+          </div>
+        ) : (
+          <div className="mt-10 flex gap-x-[5%] ">
+            <button
+              className="w-1/3 bg-green text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center"
+              onClick={saveRoleToDatabase}
+            >
+              <IoMdCheckboxOutline className="w-[22px] h-[22px]" />
+              تائید نهایی
+            </button>
+            <button
+              className="w-1/3 bg-yellowTisBee text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center"
+              onClick={editUserHandler}
+            >
+              <CiEdit className="w-[22px] h-[22px]" />
+              ویرایش اطلاعات
+            </button>
+            <button
+              className="w-1/3 bg-bgRed text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center"
+              onClick={deleteUserHandler}
+            >
+              <MdDeleteForever className="w-[22px] h-[22px]" />
+              حذف کاربر
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
 export default CardUser;
+
+// {userData.status === "pendding" ? (
+//   <div className="mt-10 flex gap-x-[5%] ">
+//     <button
+//       className="w-1/2 bg-green text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center"
+//       onClick={saveRoleToDatabase}
+//     >
+//       <IoMdCheckboxOutline className="w-[22px] h-[22px]" />
+//       تائید نهایی
+//     </button>
+//     <button
+//       className="w-1/2 bg-bgRed text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center"
+//       onClick={deleteUserHandler}
+//     >
+//       <MdDeleteForever className="w-[22px] h-[22px]" />
+//       حذف کاربر
+//     </button>
+//   </div>
+// ) : (
+//   <div className="mt-10 flex gap-x-[5%] ">
+//     <button
+//       className="w-1/3 bg-green text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center"
+//       onClick={saveRoleToDatabase}
+//     >
+//       <IoMdCheckboxOutline className="w-[22px] h-[22px]" />
+//       تائید نهایی
+//     </button>
+//     <button className="w-1/3 bg-yellowTisBee text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center" onClick={editUserHandler}>
+//       <CiEdit className="w-[22px] h-[22px]" />
+//       ویرایش اطلاعات
+//     </button>
+//     <button
+//       className="w-1/3 bg-bgRed text-textWhite p-2 text-[1.2rem] rounded-[10px] flex justify-center"
+//       onClick={deleteUserHandler}
+//     >
+//       <MdDeleteForever className="w-[22px] h-[22px]" />
+//       حذف کاربر
+//     </button>
+//   </div>
+// )}
+
+{
+  /* <div
+className={`overflow-hidden transition-all duration-500 ${
+  showDetails ? "max-h-[1000px] opacity-100" : "max-h-0 opacity-0"
+}`}
+> */
+}
+
+{
+  /* <div
+        className={`transition-max-height duration-500 ease-in-out overflow-hidden ${
+          showDetails ? "max-h-[500px]" : "max-h-0"
+        }`}
+      ></div> */
+}
