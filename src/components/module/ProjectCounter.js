d65f091f9f0f47c8a77e9701f38526c517fc222b
Mahboubeh Shoubi <mahboobeh.shobi.65@gmail.com>
2025-05-03 03:25:35 -0700

    Responsive styling in mobile size for the ProjectCounter.js component

--- src/components/module/ProjectCounter.js
+++ src/components/module/ProjectCounter.js
@@ -2,35 +2,37 @@ import Counter from "@/utils/Counter";
 
 function ProjectCounter() {
   return (
-    <div className="w-full h-[40vh] bg-secondery mb-[50px]">
-            <div className=" container w-full md:max-w-screen-xl h-full">
-              <div className="w-full flex justify-between items-center h-full">
-                <div className="w-1/3 h-[150px] flex flex-col justify-center items-center">
-                  <p className="text-textWhite text-[1.2rem]">پروژه های تمام شده</p>
-                  <p className="text-textWhite text-[2rem] h-[2rem]">
-                    <Counter max={100} time={100} />
-                  </p>
-                </div>
-                <div className="w-1/3 h-[150px] flex flex-col justify-center items-center">
-                  <p className="text-textWhite text-[1.2rem]">
-                    پروژه های در حال اجرا
-                  </p>
-                  <p className="text-textWhite text-[2rem] h-[2rem]">
-                    <Counter max={200} time={60} />
-                  </p>
-                </div>
-                <div className="w-1/3 h-[150px] flex flex-col justify-center items-center">
-                  <p className="text-textWhite text-[1.2rem]">
-                    پروژه های در حال بررسی
-                  </p>
-                  <p className="text-textWhite text-[2rem] h-[2rem]">
-                    <Counter max={300} time={40} />
-                  </p>
-                </div>
-              </div>
-            </div>
+    <div className="w-full sm:h-[40vh] mb-[50px] bg-no-repeat bg-cover bg-center" style={{
+      backgroundImage: `linear-gradient(to top , #000 ,transparent 150% ) , url(/image/counter/background-image-1.jpg)`,
+    }}>
+      <div className=" container w-full md:max-w-screen-xl sm:h-full">
+        <div className="w-full flex flex-col sm:flex-row sm:justify-between items-center h-full px-5 py-10 gap-5 sm:gap-0">
+          <div className=" w-full sm:w-1/3 h-fit py-5 sm:h-[150px] flex gap-5 sm:flex-col justify-center items-baseline sm:items-center">
+            <p className="text-textWhite text-[1.2rem]">پروژه های تمام شده</p>
+            <p className="text-textWhite text-[2.5rem] sm:text-[2rem] h-[2rem]">
+              <Counter max={100} time={100} />
+            </p>
           </div>
-  )
+          <div className=" w-full sm:w-1/3 h-fit py-5 sm:h-[150px] flex gap-5 sm:flex-col justify-center items-baseline sm:items-center">
+            <p className="text-textWhite text-[1.2rem]">
+              پروژه های در حال اجرا
+            </p>
+            <p className="text-textWhite text-[2.5rem] sm:text-[2rem] h-[2rem]">
+              <Counter max={200} time={60} />
+            </p>
+          </div>
+          <div className=" w-full sm:w-1/3 h-fit py-5 sm:h-[150px] flex gap-5 sm:flex-col justify-center items-baseline sm:items-center">
+            <p className="text-textWhite text-[1.2rem]">
+              پروژه های در حال بررسی
+            </p>
+            <p className="text-textWhite text-[2.5rem] sm:text-[2rem] h-[2rem]">
+              <Counter max={300} time={40} />
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProjectCounter
+export default ProjectCounter;
